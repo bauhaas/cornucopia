7202e21e8373c898f66e80c68f1ed45920dd0506
Korkrane <baudoin.haas@gmail.com>
2024-02-21 23:41:28 +0100

    test(): static file path for vercel

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -19,7 +19,7 @@ import { UsersModule } from './users/users.module';
     ActorsModule,
     MoviesModule,
     ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '../../', 'client', 'dist'),
+      rootPath: join(__dirname, '../../', 'client', 'dist'), // for vercel
       // rootPath: join(__dirname, '/../../../', 'client', 'dist'),
       exclude: ['/api*'],
     }),
