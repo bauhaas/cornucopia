615b2cced016fbd67070e1618485d7f4e4a281d9
Korkrane <baudoin.haas@gmail.com>
2024-03-09 01:36:01 +0100

    fix(): remove swagger static

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -28,52 +28,8 @@ async function bootstrap(): Promise<void> {
     .setVersion('1.0')
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('swagger', app, document);
+  SwaggerModule.setup('ap', app, document);
 
   await app.listen(3000);
-
-  const serverUrl = process.env.URL_LOCAL;
-  console.log(process.env.NODE_ENV);
-  // get the swagger json file (if app is running in development mode)
-  if (process.env.NODE_ENV === 'development') {
-    // write swagger ui files
-
-    if (!existsSync('swagger-static')) {
-      mkdirSync('swagger-static');
-    }
-
-    get(`${serverUrl}/swagger/swagger-ui-bundle.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-bundle.js'));
-      console.log(
-        `Swagger UI bundle file written to: '/swagger-static/swagger-ui-bundle.js'`,
-      );
-    });
-
-    get(`${serverUrl}/swagger/swagger-ui-init.js`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui-init.js'));
-      console.log(
-        `Swagger UI init file written to: '/swagger-static/swagger-ui-init.js'`,
-      );
-    });
-
-    get(
-      `${serverUrl}/swagger/swagger-ui-standalone-preset.js`,
-      function (response) {
-        response.pipe(
-          createWriteStream('swagger-static/swagger-ui-standalone-preset.js'),
-        );
-        console.log(
-          `Swagger UI standalone preset file written to: '/swagger-static/swagger-ui-standalone-preset.js'`,
-        );
-      },
-    );
-
-    get(`${serverUrl}/swagger/swagger-ui.css`, function (response) {
-      response.pipe(createWriteStream('swagger-static/swagger-ui.css'));
-      console.log(
-        `Swagger UI css file written to: '/swagger-static/swagger-ui.css'`,
-      );
-    });
-  }
 }
 bootstrap();
